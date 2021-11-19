d08189c3ce801acc6cb4e6ae324128060bb94bd2
typhlosion64 <83076588+typhlosion64@users.noreply.github.com>
2021-11-19 13:56:32 -0700

    fixed what elijah said to
    i fixed up my js file, used let, diff variable names
    made small changes to css file to make buttons look cooler
    removed a dumb <br> on my html file

--- js/home.js
+++ js/home.js
@@ -1,71 +1,76 @@
-var i = 0;
-var txt =
+let tw_index = 0;
+let homepage_txt =
   "Welcome to the GCU Technology Club's Official Web Site!"; /* The text */
-var speed = 40; /* The speed/duration of the effect in milliseconds */
+let speed = 60; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("home-welcome").innerHTML += txt.charAt(i);
-    i++;
+  if (tw_index < homepage_txt.length) {
+    document.getElementById("home-welcome").innerHTML +=
+      homepage_txt.charAt(tw_index);
+    tw_index++;
     setTimeout(typeWriter, speed);
   }
 }
 
 // Different Slides
 
-var cyber = "Cyber Security";
-var webdev = "Web Development";
-var embedded = "Embedded Development";
-var hardware = "Hardware";
+let cyber = "Cyber Security";
+let webdev = "Web Development";
+let embedded = "Embedded Development";
+let hardware = "Hardware";
 
-var p0 = "Cyber Security is doing things.";
-var p1 = "Web Dev is doing things.";
-var p2 = "Embedded Dev is doing something.";
-var p3 = "Hardware is doing something.";
+let cyber_text = "Cyber Security is doing things.";
+let webdev_text = "Web Dev is doing things.";
+let embedded_text = "Embedded Dev is doing something.";
+let hardware_text = "Hardware is doing something.";
 // code that handles the turning of pages
-var y = 0;
+
+let page_index = 0;
 function forward() {
-  y = y + 1;
-  if (y > 4) {
-    y = 4;
+  page_index = page_index + 1;
+  if (page_index > 4) {
+    page_index = 4;
     writeThis();
   } else {
     writeThis();
   }
 }
 function backward() {
-  y = y - 1;
-  if (y < 0) {
-    y = 0;
+  page_index = page_index - 1;
+  if (page_index < 0) {
+    page_index = 0;
     writeThis();
   } else {
     writeThis();
   }
 }
 // actually writes the text onto the screen
+document.getElementById("home-title").innerHTML = cyber;
+document.getElementById("home-text").innerHTML = cyber_text;
+document.getElementById("home-backward-button").style.visibility = "hidden";
 
 function writeThis() {
-  if (y == 0) {
+  if (page_index == 0) {
     document.getElementById("home-title").innerHTML = cyber;
-    document.getElementById("home-text").innerHTML = p0;
+    document.getElementById("home-text").innerHTML = cyber_text;
     document.getElementById("home-backward-button").style.visibility = "hidden";
     document.getElementById("home-forward-button").style.visibility = "visible";
   }
-  if (y == 1) {
+  if (page_index == 1) {
     document.getElementById("home-title").innerHTML = webdev;
-    document.getElementById("home-text").innerHTML = p1;
+    document.getElementById("home-text").innerHTML = webdev_text;
     document.getElementById("home-backward-button").style.visibility =
       "visible";
   }
-  if (y == 2) {
+  if (page_index == 2) {
     document.getElementById("home-title").innerHTML = embedded;
-    document.getElementById("home-text").innerHTML = p2;
+    document.getElementById("home-text").innerHTML = embedded_text;
     document.getElementById("home-forward-button").style.visibility = "visible";
   }
 
-  if (y == 3) {
+  if (page_index == 3) {
     document.getElementById("home-title").innerHTML = hardware;
-    document.getElementById("home-text").innerHTML = p3;
+    document.getElementById("home-text").innerHTML = hardware_text;
     document.getElementById("home-forward-button").style.visibility = "hidden";
   }
 }
