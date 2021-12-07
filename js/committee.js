a9a664b13f42676932c545c06f5dea37ca0ac7bc
shazebs <shazebs7@outlook.com>
2021-12-07 02:20:38 -0700

    Fixed more css styling, adding more @media responsiveness, created a 'highlightSelection' function, did some navbar @media responsiveness styling, and styled the committee text content paragraphs.

--- js/committee.js
+++ js/committee.js
@@ -1,3 +1,25 @@
+
+var committees = new Array('web-dev', 
+                            'embedded',
+                            'cyber',
+                            'girls-who-code',
+                            'hardware'); 
+
+// Left div selection highlight function. 
+function highlightSelection(turnOn) 
+{
+    for(let i = 0; i < committees.length; i++)
+    {
+        if (committees[i] == turnOn) {
+            let on = document.getElementById(turnOn).style.backgroundColor = '#A68AC8';
+        }
+        else {
+            let off = document.getElementById(committees[i]).style.backgroundColor = '#774BAA';
+        }
+    }
+} 
+
+
 function showWebDev(){
 
     // Select Right Div content components. 
@@ -8,11 +30,13 @@ function showWebDev(){
     title.innerText = 'Web Development Committee'; 
 
     // create markup to write to page. 
-    var codeline = '<img class="content" src="images/img1.png"><br>' + 
-    $("#info-webdevelopment").text(); 
+    var codeline = '<img class="content" src="images/img1.png"><br><br><div>' + 
+    $("#info-webdevelopment").text() + '</div><br>'; 
 
     // write the markup. 
     body.innerHTML = codeline;  
+
+    highlightSelection("web-dev"); 
 }
 
 
@@ -26,10 +50,10 @@ function showEmbedded()
     title.innerText = 'Embedded Development Committee'; 
 
     // Write markup to the page. 
-    body.innerHTML = '<img class="content" src="https://www.ssla.co.uk/wp-content/uploads/2020/10/embedded-software-entwickler-entwicklung-tasks.png"/><br>' + 
-    $("#info-embeddeddevelopment").text(); 
+    body.innerHTML = '<img class="content" src="https://www.ssla.co.uk/wp-content/uploads/2020/10/embedded-software-entwickler-entwicklung-tasks.png"/><br><br><div>' + 
+    $("#info-embeddeddevelopment").text() + '</div><br>'; 
 
-
+    highlightSelection("embedded");
 }
 
 function showCyber()
@@ -42,31 +66,47 @@ function showCyber()
     title.innerText = 'Cybersecurity Committee'; 
 
     // Create some markup to write to page. 
-    let codeline = '<img class="content" src="images/cyber_img.png"><br>' + 
-    $("#info-cybersecurity").text();
+    let codeline = '<img class="content" src="images/cyber_img.png"><br><br><div>' + $("#info-cybersecurity").text() + '</div><br>';
 
     // Write markup to the document. 
     body.innerHTML = codeline; 
+
+    highlightSelection('cyber');
 }
 
 function showGWC(){
     var title = document.getElementById('content-title'); 
     var body = document.getElementById('content-body'); 
+
+    // Set the right div title. 
     title.innerText = 'Girls Who Code Committee'; 
 
     // Create some markup to page. 
-    var codeline = '<img class="content" src="https://cdn.myanimelist.net/r/560x300/s/common/uploaded_files/1472794821-841ff512db1def20fba45c98328bc924.jpeg?s=d5e3294311d6f81d4731142b3681302f"><br>'; 
+    var codeline = '<img class="content" src="https://cdn.myanimelist.net/r/560x300/s/common/uploaded_files/1472794821-841ff512db1def20fba45c98328bc924.jpeg?s=d5e3294311d6f81d4731142b3681302f"><br><br><div>' + 
+    $("#info-girlswhocode").text() + '</div><br>'; 
 
     // Write markup to the document. 
     body.innerHTML = codeline; 
+
+    highlightSelection('girls-who-code');
 }
 
 function showHardware(){
     var title = document.getElementById('content-title'); 
     var body = document.getElementById('content-body'); 
+
+    // Set the right div title. 
     title.innerText = 'Hardware Committee'; 
 
-    var codeline = '<img class="content" src="https://www.lifewire.com/thmb/KW1l5x2qWzGVHf5Fl7yAD8OwwcQ=/3000x2000/filters:fill(auto,1)/computer-hardware-2625895-final-v1-8c909b8a32434e26a225db2314823bb2.jpg"><br>'; 
+    // Create some markup.
+    var codeline = '<img class="content" src="https://www.lifewire.com/thmb/KW1l5x2qWzGVHf5Fl7yAD8OwwcQ=/3000x2000/filters:fill(auto,1)/computer-hardware-2625895-final-v1-8c909b8a32434e26a225db2314823bb2.jpg"><br><br><div>' + 
+    $("#info-hardware").text() + '</div><br>'; 
     
+    // Write markup to page. 
     body.innerHTML = codeline; 
+
+    highlightSelection('hardware');
 }
+
+
+
