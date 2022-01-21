e4560fedff688112882bc178abf197f978fc7674
shazebs <shazebs7@outlook.com>
2022-01-20 18:00:06 -0700

    Added in comments (no serious code changes)

--- js/committee.js
+++ js/committee.js
@@ -1,7 +1,5 @@
 
-/*
-Array of all committees. 
-*/
+/* Array of all committees.  */
 const committees = [
 	'web-dev',
 	'embedded',
@@ -10,41 +8,53 @@ const committees = [
 	'hardware',
 ]; 
 
-// Left div selection highlight function. 
+
+
+
+// Left div (tab highlighting) function. 
 function highlightSelection(turnOn) 
 {
+    // which tab in the left column is currently selected
     for(const committee of committees)
     {
+        // turn on highlight
         if (committee == turnOn) {
             document.getElementById(turnOn).style.backgroundColor = '#A68AC8';
         }
-        else {
+        else { // turn off highlight 
             document.getElementById(committee).style.backgroundColor = '#774BAA';
         }
     }
 } 
 
 
-function showWebDev(){
 
-    // Select Right Div content components. 
+/*          Committees Body Contents              */ 
+
+// Web Development Data Initialization function 
+function showWebDev()
+{
+    // Select Right Div title and body (see variable names) 
     const title = document.getElementById('content-title');
     const body = document.getElementById('content-body'); 
 
-    // Set the Content Title. 
+    // Set the Content Title value via innerText() method
     title.innerText = 'Web Development Committee'; 
 
-    // create markup to write to page. 
+    // create markup that sets an image and 
     let codeline = '<img class="content" src="images/img1.png"><br><br><div>' + 
+    // cool way to query select an id on the document 
     $("#info-webdevelopment").text() + '</div><br>'; 
 
-    // write the markup. 
+    // write the markup to document 
     body.innerHTML = codeline;  
 
+    // make sure web dev is highlighted 
     highlightSelection("web-dev"); 
 }
 
 
+// Embedded Development Data Initialization function 
 function showEmbedded() 
 {
     // Select Right Div title and body sections. 
@@ -66,9 +76,12 @@ function showEmbedded()
     //$("#info-embeddeddevelopment").text() + '</div><br>'; 
     body.replaceChildren(img, document.createElement('br'), document.createElement('br'), desc);
 
+    // highlight embedded tab 
     highlightSelection("embedded");
 }
 
+
+// Cybersecurity Data is initialized with this function 
 function showCyber()
 {
     // Select content title and body elements. 
@@ -78,46 +91,57 @@ function showCyber()
     // Set Content title. 
     title.innerText = 'Cybersecurity Committee'; 
 
-    // Create some markup to write to page. 
+    // Create some markup to write to page displaying an image and text via query select
     let codeline = '<img class="content" src="images/cyber_img.png"><br><br><div>' + $("#info-cybersecurity").text() + '</div><br>';
 
     // Write markup to the document. 
     body.innerHTML = codeline; 
 
+    // highlight cyber tab
     highlightSelection('cyber');
 }
 
-function showGWC(){
+
+// Girls who code data initialization function 
+function showGWC()
+{
+    // query select by id the right column title and description 
     let title = document.getElementById('content-title'); 
     let body = document.getElementById('content-body'); 
 
     // Set the right div title. 
     title.innerText = 'Girls Who Code Committee'; 
 
-    // Create some markup to page. 
+    // Create some markup to page displaying the committee image and description 
     let codeline = '<img class="content" src="images/gwc_logo.png"><br><br><div>' + 
     $("#info-girlswhocode").text() + '</div><br>'; 
 
     // Write markup to the document. 
     body.innerHTML = codeline; 
 
+    // highlight girls who code tab 
     highlightSelection('girls-who-code');
 }
 
-function showHardware(){
+
+// Hardware data initialization function 
+function showHardware()
+{
+    // select right column title and text boxes
     let title = document.getElementById('content-title'); 
     let body = document.getElementById('content-body'); 
 
-    // Set the right div title. 
+    // Set the right column div title. 
     title.innerText = 'Hardware Committee'; 
 
-    // Create some markup.
+    // Create some markup displaying committee image and text 
     let codeline = '<img class="content" src="https://www.lifewire.com/thmb/KW1l5x2qWzGVHf5Fl7yAD8OwwcQ=/3000x2000/filters:fill(auto,1)/computer-hardware-2625895-final-v1-8c909b8a32434e26a225db2314823bb2.jpg"><br><br><div>' + 
     $("#info-hardware").text() + '</div><br>'; 
     
     // Write markup to page. 
     body.innerHTML = codeline; 
 
+    // highlight hardware tab 
     highlightSelection('hardware');
 }
 
