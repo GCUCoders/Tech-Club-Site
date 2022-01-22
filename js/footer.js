30517e627d06d7c7c5cedcdd44d0f427717cb026
ShadowmanV92 <alexpeltier@hotmail.com>
2022-01-22 11:24:06 -0700

    Remove testing, add footer web component
    This JavaScript stuff is pretty neat.

--- js/footer.js
+++ js/footer.js
@@ -1,53 +1,27 @@
 import {LitElement, html, css} from 'lit-element';
 
-class ProjectCard extends LitElement{
-    // defined css styling
+class FooterWebComponent extends LitElement {
+    // CSS style sheet 
     static get styles(){
         return css`
-            .card{
-                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-                transition: 0.3s;
-                border-radius: 5px;
-                width: 300px;
-            }
-            .card:hover{
-                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-            }
-            .container  {
-                padding: 2px 16px;
-            }
-        `;
+        #footer-w-c{
+            background-color:black; 
+            color:white; 
+            text-align:center; 
+            padding:10px 0px 30px 0px; 
+        }`;
     }
-
 
     render(){
         return html`
-            <div class="card">
-                <img src="${this.img}" width=300px>
-
-                <div class="container">
-                    <h3> <b> ${this.name} </b> </h3>
-                    <p> <a href="${this.url}"> Project Link </a></p>
-                </div>
-            </div>
-        `;
+        <footer ID="footer-w-c">Grand Canyon University 2021 | All Rights Reserved.</footer>
+        `
     }
 
     constructor(){
         super();
-        this.name = "Project Name";
-        this.url = "https://google.com";
-        this.img = "https://img.icons8.com/cotton/2x/folder-invoices.png"
-    }
-
-    static get properties(){
-        return{
-            name: { type: String },
-            url: { type: String},
-            img: { type: String}
-        };
     }
 
 }
 
-customElements.define('project-card', ProjectCard);
+customElements.define('footer-w-c', FooterWebComponent);
