aacb4f50e579f02cd35391266c4f217cf31ddf8a
admeeer <alexpeltier@hotmail.com>
2022-01-24 18:26:07 -0700

    Update update
    Don't read this. I am internally coping.

--- js/footer.js
+++ js/footer.js
@@ -1,53 +1,36 @@
 import {LitElement, html, css} from 'lit-element';
 
-class ProjectCard extends LitElement{
+class Footer extends LitElement{
     // defined css styling
     static get styles(){
         return css`
-            .card{
-                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-                transition: 0.3s;
-                border-radius: 5px;
-                width: 300px;
-            }
-            .card:hover{
-                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-            }
-            .container  {
-                padding: 2px 16px;
-            }
+           #footer-heading {
+               /* standard font */
+                font-family: Helvetica, sans-serif;
+                font-weight: bold;
+
+                background-color: black;
+                color: white;
+                text-align: center;
+                /* innovation. */
+                padding:22px 0px 22px 0px; 
+                
+           }
         `;
     }
 
 
     render(){
         return html`
-            <div class="card">
-                <img src="${this.img}" width=300px>
-
-                <div class="container">
-                    <h3> <b> ${this.name} </b> </h3>
-                    <p> <a href="${this.url}"> Project Link </a></p>
-                </div>
-            </div>
-        `;
+<footer>
+<div id="footer-heading">Ⓒ Grand Canyon University</div>
+</footer>`;
     }
 
     constructor(){
         super();
-        this.name = "Project Name";
-        this.url = "https://google.com";
-        this.img = "https://img.icons8.com/cotton/2x/folder-invoices.png"
-    }
-
-    static get properties(){
-        return{
-            name: { type: String },
-            url: { type: String},
-            img: { type: String}
-        };
     }
 
 }
 
-customElements.define('project-card', ProjectCard);
+customElements.define('footer-ss', Footer);
