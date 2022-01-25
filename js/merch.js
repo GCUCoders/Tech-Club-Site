17561a02c733540d4e4234e49cfea019cb280f98
ponyboy14 <lucaslasecla44@gmail.com>
2022-01-24 18:52:33 -0700

    Create merch.js
    add mock data

---
+++ js/merch.js
@@ -0,0 +1,411 @@
+const merch_items= [{
+    "id": 1,
+    "name": "Cereopsis novaehollandiae",
+    "price": 25.47,
+    "image": "http://dummyimage.com/163x100.png/ff4444/ffffff",
+    "description": "Closed [percutaneous] [needle] biopsy of cranial or peripheral nerve or ganglion",
+    "categories": [
+      "Nissan",
+      "Hyundai"
+    ],
+    "sizes": [
+      {
+        "size": "XL",
+        "quantity": 8
+      },
+      {
+        "size": "S",
+        "quantity": 12
+      },
+      {
+        "size": "XS",
+        "quantity": 10
+      },
+      {
+        "size": "3XL",
+        "quantity": 6
+      }
+    ]
+  }, {
+    "id": 2,
+    "name": "Stercorarius longicausus",
+    "price": 34.39,
+    "image": "http://dummyimage.com/221x100.png/ff4444/ffffff",
+    "description": "Muscle transfer or transplantation",
+    "categories": [
+      "Ford",
+      "Chevrolet",
+      "Pontiac",
+      "Ford"
+    ],
+    "sizes": [
+      {
+        "size": "S",
+        "quantity": 3
+      },
+      {
+        "size": "3XL",
+        "quantity": 17
+      },
+      {
+        "size": "L",
+        "quantity": 17
+      },
+      {
+        "size": "M",
+        "quantity": 15
+      }
+    ]
+  }, {
+    "id": 3,
+    "name": "Paraxerus cepapi",
+    "price": 34.44,
+    "image": "http://dummyimage.com/197x100.png/cc0000/ffffff",
+    "description": "Transplant from cadaver",
+    "categories": [
+      "Chevrolet"
+    ],
+    "sizes": [
+      {
+        "size": "2XL",
+        "quantity": 20
+      }
+    ]
+  }, {
+    "id": 4,
+    "name": "Marmota caligata",
+    "price": 49.79,
+    "image": "http://dummyimage.com/111x100.png/cc0000/ffffff",
+    "description": "Other local destruction or excision of renal lesion or tissue",
+    "categories": [
+      "Lincoln"
+    ],
+    "sizes": [
+      {
+        "size": "3XL",
+        "quantity": 2
+      },
+      {
+        "size": "3XL",
+        "quantity": 4
+      }
+    ]
+  }, {
+    "id": 5,
+    "name": "Ara ararauna",
+    "price": 29.91,
+    "image": "http://dummyimage.com/144x100.png/5fa2dd/ffffff",
+    "description": "Endometrial ablation",
+    "categories": [
+      "Volkswagen",
+      "Ford",
+      "Chevrolet",
+      "Cadillac"
+    ],
+    "sizes": [
+      {
+        "size": "L",
+        "quantity": 20
+      },
+      {
+        "size": "2XL",
+        "quantity": 8
+      }
+    ]
+  }, {
+    "id": 6,
+    "name": "Felis libyca",
+    "price": 37.48,
+    "image": "http://dummyimage.com/103x100.png/ff4444/ffffff",
+    "description": "Microscopic examination of specimen from operative wound, cell block and Papanicolaou smear",
+    "categories": [
+      "Audi",
+      "Chevrolet",
+      "Dodge",
+      "Audi",
+      "GMC"
+    ],
+    "sizes": [
+      {
+        "size": "2XL",
+        "quantity": 5
+      },
+      {
+        "size": "3XL",
+        "quantity": 20
+      },
+      {
+        "size": "M",
+        "quantity": 12
+      },
+      {
+        "size": "XL",
+        "quantity": 12
+      }
+    ]
+  }, {
+    "id": 7,
+    "name": "Vanellus chilensis",
+    "price": 34.05,
+    "image": "http://dummyimage.com/127x100.png/cc0000/ffffff",
+    "description": "Other bilateral ligation and division of fallopian tubes",
+    "categories": [
+      "Chevrolet",
+      "Volkswagen",
+      "Nissan",
+      "Chevrolet",
+      "Toyota"
+    ],
+    "sizes": [
+      {
+        "size": "M",
+        "quantity": 14
+      },
+      {
+        "size": "S",
+        "quantity": 3
+      }
+    ]
+  }, {
+    "id": 8,
+    "name": "Prionace glauca",
+    "price": 48.77,
+    "image": "http://dummyimage.com/182x100.png/ff4444/ffffff",
+    "description": "Injection of therapeutic substance into tendon of hand",
+    "categories": [
+      "Toyota",
+      "Dodge",
+      "Lincoln"
+    ],
+    "sizes": [
+      {
+        "size": "XS",
+        "quantity": 19
+      },
+      {
+        "size": "S",
+        "quantity": 10
+      },
+      {
+        "size": "2XL",
+        "quantity": 13
+      },
+      {
+        "size": "XL",
+        "quantity": 3
+      },
+      {
+        "size": "XL",
+        "quantity": 15
+      }
+    ]
+  }, {
+    "id": 9,
+    "name": "Salvadora hexalepis",
+    "price": 33.57,
+    "image": "http://dummyimage.com/210x100.png/cc0000/ffffff",
+    "description": "Incision of large intestine",
+    "categories": [
+      "Infiniti",
+      "Lincoln"
+    ],
+    "sizes": [
+      {
+        "size": "L",
+        "quantity": 11
+      },
+      {
+        "size": "L",
+        "quantity": 7
+      },
+      {
+        "size": "XS",
+        "quantity": 8
+      },
+      {
+        "size": "M",
+        "quantity": 10
+      },
+      {
+        "size": "2XL",
+        "quantity": 17
+      }
+    ]
+  }, {
+    "id": 10,
+    "name": "Thalasseus maximus",
+    "price": 25.58,
+    "image": "http://dummyimage.com/151x100.png/dddddd/000000",
+    "description": "Implantation of artificial urinary sphincter [AUS]",
+    "categories": [
+      "Dodge"
+    ],
+    "sizes": [
+      {
+        "size": "M",
+        "quantity": 6
+      },
+      {
+        "size": "S",
+        "quantity": 10
+      },
+      {
+        "size": "L",
+        "quantity": 13
+      },
+      {
+        "size": "M",
+        "quantity": 20
+      }
+    ]
+  }, {
+    "id": 11,
+    "name": "Myotis lucifugus",
+    "price": 26.9,
+    "image": "http://dummyimage.com/150x100.png/ff4444/ffffff",
+    "description": "Resection of vessel with anastomosis, abdominal arteries",
+    "categories": [
+      "Nissan",
+      "Suzuki",
+      "Ford",
+      "Dodge"
+    ],
+    "sizes": [
+      {
+        "size": "M",
+        "quantity": 8
+      },
+      {
+        "size": "2XL",
+        "quantity": 11
+      }
+    ]
+  }, {
+    "id": 12,
+    "name": "Dusicyon thous",
+    "price": 7.91,
+    "image": "http://dummyimage.com/124x100.png/dddddd/000000",
+    "description": "Other total excision of thymus",
+    "categories": [
+      "Mercedes-Benz",
+      "Suzuki",
+      "Dodge",
+      "GMC",
+      "Buick"
+    ],
+    "sizes": [
+      {
+        "size": "S",
+        "quantity": 15
+      },
+      {
+        "size": "3XL",
+        "quantity": 11
+      },
+      {
+        "size": "S",
+        "quantity": 8
+      },
+      {
+        "size": "XS",
+        "quantity": 5
+      },
+      {
+        "size": "XS",
+        "quantity": 6
+      }
+    ]
+  }, {
+    "id": 13,
+    "name": "Mungos mungo",
+    "price": 28.87,
+    "image": "http://dummyimage.com/142x100.png/dddddd/000000",
+    "description": "Microscopic examination of specimen from ear, nose, throat, and larynx, culture",
+    "categories": [
+      "Volkswagen",
+      "Mercury",
+      "Mazda",
+      "Ford"
+    ],
+    "sizes": [
+      {
+        "size": "3XL",
+        "quantity": 5
+      },
+      {
+        "size": "2XL",
+        "quantity": 4
+      },
+      {
+        "size": "L",
+        "quantity": 11
+      },
+      {
+        "size": "S",
+        "quantity": 12
+      },
+      {
+        "size": "XL",
+        "quantity": 9
+      }
+    ]
+  }, {
+    "id": 14,
+    "name": "Bubalornis niger",
+    "price": 23.77,
+    "image": "http://dummyimage.com/211x100.png/dddddd/000000",
+    "description": "Microscopic examination of blood, toxicology",
+    "categories": [
+      "Jeep",
+      "Hyundai",
+      "Pontiac",
+      "Chevrolet",
+      "Subaru"
+    ],
+    "sizes": [
+      {
+        "size": "M",
+        "quantity": 18
+      },
+      {
+        "size": "3XL",
+        "quantity": 14
+      },
+      {
+        "size": "L",
+        "quantity": 10
+      },
+      {
+        "size": "S",
+        "quantity": 9
+      }
+    ]
+  }, {
+    "id": 15,
+    "name": "Hystrix indica",
+    "price": 45.83,
+    "image": "http://dummyimage.com/194x100.png/5fa2dd/ffffff",
+    "description": "Laparoscopic vertical (sleeve) gastrectomy",
+    "categories": [
+      "Ford",
+      "Hummer",
+      "Dodge",
+      "Audi"
+    ],
+    "sizes": [
+      {
+        "size": "XS",
+        "quantity": 1
+      },
+      {
+        "size": "XL",
+        "quantity": 5
+      }
+    ]
+  }],
+
+  const template = `
+<div id="${item.id}" class="pic-div">
+    <img src="images/TCLBlack-bg.png" class="merch-product" alt="Product 1">
+    <a href="/products/${item.id}">${item.name}<br>${item.price}</a>
+</div>`;
