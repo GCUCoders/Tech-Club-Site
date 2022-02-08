230b0841fb3efadc2a14475f6dacddbf3e7128ab
elijaholmos <eli.olmos9@gmail.com>
2022-02-07 18:34:03 -0700

    add database retrieval

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
 
 const app = initializeApp({
 	apiKey: 'AIzaSyBUjgwLzmnqM7O3LSfSiIIQQogIvu8XBZc',
@@ -11,14 +12,19 @@ const app = initializeApp({
 	appId: '1:1040213454604:web:a638bfdfd26e4e7795a329',
 });
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 
 const login = async function (e) {
     e.preventDefault();
     console.log(e);
     console.log('In login');
-    const user = await signInWithEmailAndPassword(auth, e.target.email.value, e.target.password.value);
-    console.log(user);
+    await signInWithEmailAndPassword(auth, e.target.email.value, e.target.password.value);
+    //console.log(user);	
+};
+
+const getHomepageContent = async function () {
+	return (await get(ref(db, 'homepage/committees'))).val();
 };
 
 document.querySelector('#admin-form').addEventListener('submit', login);
