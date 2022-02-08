1ab5ec84c1d5964f81307c49409c72944746c01d
shazebs <shazebs7@outlook.com>
2022-02-07 18:07:46 -0700

    Fixed committee.js code to be more DOM appropriate

--- js/committee.js
+++ js/committee.js
@@ -9,7 +9,7 @@ const committees = [
 ]; 
 
 
-// Left div (tab highlighting) function. 
+// Left div column (tab highlighting) function. 
 function highlightSelection(turnOn) 
 {
     // which tab in the left column is currently selected
@@ -28,117 +28,170 @@ function highlightSelection(turnOn)
 
 
 
-/*          Committees Body Contents              */ 
-
-// Web Development Data Initialization function 
+/*          Web-Development Body Content function             */ 
 function showWebDev()
 {
-    // Select Right Div title and body (see variable names) 
+    // select content title and body elements
     const title = document.getElementById('content-title');
     const body = document.getElementById('content-body'); 
-
-    // Set the Content Title value via innerText() method
+    
+    // set content title 
     title.innerText = 'Web Development Committee'; 
 
+<<<<<<< Updated upstream
     // create markup that sets an image and 
     let codeline = '<img class="content" src="images/webdev_customImg.png"><br><br><div>' + 
     // cool way to query select an id on the document 
     $("#info-webdevelopment").text() + '</div>'; 
+=======
+    // create image element
+    const img = document.createElement('img'); 
+    img.classList.add('content'); 
+    img.src = 'images/webdev_customImg.png'; 
+>>>>>>> Stashed changes
 
-    // write the markup to document 
-    body.innerHTML = codeline;  
+    // create description element
+    const description = document.createElement('div'); 
+    description.innerHTML = $('#info-webdevelopment').text(); 
 
-    // make sure web dev is highlighted 
+    // add elements into content node 
+    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), description); 
+
+    // highlight web-development tab 
     highlightSelection("web-dev"); 
 }
 
 
-// Embedded Development Data Initialization function 
+
+/*          Embedded-Development Body Content function              */ 
 function showEmbedded() 
 {
-    // Select Right Div title and body sections. 
+    // select content title and body elements
     let body = document.getElementById('content-body'); 
     let title = document.getElementById('content-title'); 
     
-    // Set content title. 
+    // set content title 
     title.innerText = 'Embedded Development Committee'; 
 
-    // Write markup to the page. 
+    // create image element 
     const img = document.createElement('img');
     img.classList.add('content');
     img.src = 'https://www.ssla.co.uk/wp-content/uploads/2020/10/embedded-software-entwickler-entwicklung-tasks.png';
 
+<<<<<<< Updated upstream
     const desc = document.createElement('div');
     desc.innerHTML = $("#info-embeddeddevelopment").text();
+=======
+    // create description element
+    const description = document.createElement('div');
+    description.innerHTML = $("#info-embeddeddevelopment").text();
+>>>>>>> Stashed changes
 
-    //body.innerHTML = '<img class="content" src="https://www.ssla.co.uk/wp-content/uploads/2020/10/embedded-software-entwickler-entwicklung-tasks.png"/><br><br><div>' + 
-    //$("#info-embeddeddevelopment").text() + '</div><br>'; 
-    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), desc);
+    // add elements into content node 
+    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), description);
 
-    // highlight embedded tab 
+    // highlight embedded-development tab 
     highlightSelection("embedded");
 }
 
 
-// Cybersecurity Data is initialized with this function 
+
+/*          Cybersecurity Body Content function              */ 
 function showCyber()
 {
-    // Select content title and body elements. 
+    // select content title and body elements
     let title = document.getElementById('content-title'); 
     let body = document.getElementById('content-body'); 
 
-    // Set Content title. 
+    // set content title  
     title.innerText = 'Cybersecurity Committee'; 
 
+<<<<<<< Updated upstream
     // Create some markup to write to page displaying an image and text via query select
     let codeline = '<img class="content" src="images/cyber_img.png"><br><br><div>' + $("#info-cybersecurity").text() + '</div>';
+=======
+    // create image element 
+    const img = document.createElement('img');
+    img.classList.add('content');
+    img.src = 'images/cyber_img.png';
 
-    // Write markup to the document. 
-    body.innerHTML = codeline; 
+    // create description element
+    const description = document.createElement('div');
+    description.innerHTML = $("#info-cybersecurity").text();
+>>>>>>> Stashed changes
 
-    // highlight cyber tab
+    // add elements into content node 
+    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), description);
+
+    // highlight cybersecurity tab
     highlightSelection('cyber');
 }
 
 
-// Girls who code data initialization function 
+
+/*          Girls-Who-Code Body Content function              */
 function showGWC()
 {
-    // query select by id the right column title and description 
+    // select title and body elements
     let title = document.getElementById('content-title'); 
     let body = document.getElementById('content-body'); 
 
-    // Set the right div title. 
+    // set content title 
     title.innerText = 'Girls Who Code Committee'; 
 
+<<<<<<< Updated upstream
     // Create some markup to page displaying the committee image and description 
     let codeline = '<img class="content" src="images/gwc_logo1.png"><br><br><div>' + 
     $("#info-girlswhocode").text() + '<br>Check out their website at:  <a style="color:white; font-size:bolder;" href="https://girlswhocode.com/" target="_blank">www.girlswhocode.com</a></div>'; 
+=======
+    // create image element 
+    const img = document.createElement('img');
+    img.classList.add('content');
+    img.src = 'images/gwc_logo1.png';
+>>>>>>> Stashed changes
 
-    // Write markup to the document. 
-    body.innerHTML = codeline; 
+    // create description element
+    const description = document.createElement('div');
+    description.innerHTML = $("#info-girlswhocode").text() + '<br>Check out their website at:  <a style="color:white; font-size:bolder;" href="https://girlswhocode.com/" target="_blank">www.girlswhocode.com</a></div>';
+
+    // add elements into content node 
+    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), description);
 
     // highlight girls who code tab 
     highlightSelection('girls-who-code');
 }
 
 
-// Hardware data initialization function 
+/*          Hardware Content function              */ 
 function showHardware()
 {
-    // select right column title and text boxes
+    // select content title and body 
     let title = document.getElementById('content-title'); 
     let body = document.getElementById('content-body'); 
 
     // Set the right column div title. 
     title.innerText = 'Hardware Committee'; 
 
+<<<<<<< Updated upstream
     // Create some markup displaying committee image and text 
     let codeline = '<img class="content" src="images/hardware-committee-img.png"><br><br><div>' + 
     $("#info-hardware").text() + '</div>'; 
     
     // Write markup to page. 
     body.innerHTML = codeline; 
+=======
+    // create image element 
+    const img = document.createElement('img');
+    img.classList.add('content');
+    img.src = 'https://www.lifewire.com/thmb/KW1l5x2qWzGVHf5Fl7yAD8OwwcQ=/3000x2000/filters:fill(auto,1)/computer-hardware-2625895-final-v1-8c909b8a32434e26a225db2314823bb2.jpg';
+
+    // create description element
+    const description = document.createElement('div');
+    description.innerHTML = $("#info-hardware").text();
+
+    // add elements into content node 
+    body.replaceChildren(img, document.createElement('br'), document.createElement('br'), description);
+>>>>>>> Stashed changes
 
     // highlight hardware tab 
     highlightSelection('hardware');
