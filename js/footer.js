1c70a60a530389625282bbd7d794cd94de37e118
ShadowmanV92 <alexpeltier@hotmail.com>
2022-02-17 19:28:33 -0700

    Add footer
    footer

--- js/footer.js
+++ js/footer.js
@@ -1,38 +1,39 @@
-import {LitElement, html, css} from 'lit-element';
+import {LitElement, html, css} from 'lit';
 
 class Footer extends LitElement{
     // defined css styling
     static get styles(){
         return css`
-            footer {
-                position: static;
-                bottom: 0;
-                width: 100%;
-            }
-           #footer-div {
-               /* standard font */
-                font-family: Helvetica, sans-serif;
-                font-size: 15px;
-                /* colors and stuff */
-                background-color: black;
-                color: white;
-                /* positioning for flex */
-                display: flex;
-                flex-basis: 50%;
-                align-items: center;
-                flex-direction: column;
+        footer {
+            position: static;
+            bottom: 0;
+            width: 100%;
+        }
+       #footer-div {
+           /* standard font */
+            font-family: Helvetica, sans-serif;
+            font-size: 117%;
+            font-weight: 500;
+            /* colors and stuff */
+            background-color: black;
+            color: white;
+            /* positioning for flex */
+            display: flex;
+            flex-basis: 50%;
+            align-items: center;
+            flex-direction: column;
 
-                /* innovation. */
-                padding:22px 0px 22px 0px; 
-                
-           }
-           .footer-images {
-               /* Yes david, I know, I know.*/
-               width: 27px;
-               margin-left: 11px;
-               margin-right: 11px;
-           }
-        `;
+            /* innovation. */
+            padding:22px 0px 22px 0px; 
+            
+       }
+       .footer-images {
+           /* Yes david, I know, I know.*/
+           width: 33px;
+           margin-left: 11px;
+           margin-right: 11px;
+       }
+       `;
     }
 
 
