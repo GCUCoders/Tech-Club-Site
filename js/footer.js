4fc90f6a2c5c7157f5534df1ce909805e3106e4c
elijaholmos <eli.olmos9@gmail.com>
2022-02-21 18:19:15 -0700

    implement static footer using Lit components

--- js/footer.js
+++ js/footer.js
@@ -1,68 +1,68 @@
-import {LitElement, html, css} from 'lit';
+import { LitElement, html, css } from 'https://cdn.skypack.dev/pin/lit@v2.2.0-SekDu8dt49Zcuxzc30cK/mode=imports,min/optimized/lit.js';
 
-class Footer extends LitElement{
-    // defined css styling
-    static get styles(){
-        return css`
-        footer {
-            position: static;
-            bottom: 0;
-            width: 100%;
-        }
-       #footer-div {
-           /* standard font */
-            font-family: Helvetica, sans-serif;
-            font-size: 117%;
-            font-weight: 500;
-            /* colors and stuff */
-            background-color: black;
-            color: white;
-            /* positioning for flex */
-            display: flex;
-            flex-basis: 50%;
-            align-items: center;
-            flex-direction: column;
+class Footer extends LitElement {
+	// defined css styling
+	static get styles() {
+		return css`
+			footer {
+				position: static;
+				bottom: 0;
+				width: 100%;
+			}
+			#footer-div {
+				/* standard font */
+				font-family: Helvetica, sans-serif;
+				font-size: 117%;
+				font-weight: 500;
+				/* colors and stuff */
+				background-color: black;
+				color: white;
+				/* positioning for flex */
+				display: flex;
+				flex-basis: 50%;
+				align-items: center;
+				flex-direction: column;
 
-            /* innovation. */
-            padding:22px 0px 22px 0px; 
-            
-       }
-       .footer-images {
-           /* Yes david, I know, I know.*/
-           width: 33px;
-           margin-left: 11px;
-           margin-right: 11px;
-       }
-       `;
-    }
+				/* innovation. */
+				padding: 22px 0px 22px 0px;
+			}
+			.footer-images {
+				/* Yes david, I know, I know.*/
+				width: 33px;
+				margin-left: 11px;
+				margin-right: 11px;
+			}
+		`;
+	}
 
+	render() {
+		return html`<footer>
+			<div id="footer-div">
+				<p>© Grand Canyon University</p>
+				<p>
+					<a href="${this.imgsLinks[0]}" target="_blank">
+                        <img src="${this.imgs[0]}" class="footer-images"/>
+                    </a>
+					<a href="${this.imgsLinks[1]}" target="_blank">
+                        <img src="${this.imgs[1]}" class="footer-images"/>
+                    </a>
+				</p>
+			</div>
+		</footer>`;
+	}
 
-    render(){
-        return html`
-<footer>
-<div id="footer-div">
-<p>© Grand Canyon University</p>
-<p>
-<a href="${this.imgsLinks[0]}"><img src="${this.imgs[0]}" class="footer-images"></a>
-<a href="${this.imgsLinks[1]}"><img src ="${this.imgs[1]}" class="footer-images"></a> 
-</p>
-</div>
-</footer>`;
-    }
+	constructor() {
+		super();
+		this.imgs = [];
+		this.imgsLinks = [];
+	}
 
-    constructor(){
-        super();
-        this.imgs = [];
-        this.imgsLinks = [];
-    }
-
-    static get properties(){
-        return{
-            imgs: { type: Array},
-            imgsLinks: { type: Array}
-        };
-    }
-
+	static get properties() {
+		return {
+			imgs: { type: Array },
+			imgsLinks: { type: Array },
+		};
+	}
 }
 
 customElements.define('footer-ss', Footer);
